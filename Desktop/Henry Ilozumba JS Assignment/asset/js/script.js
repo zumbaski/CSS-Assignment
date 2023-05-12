3ad69e7938d5b3cf9d1a9fcbf80dc23efa3d69a5
Zumbaski <henryilozumba@gmail.com>
2023-05-12 03:32:57 +0100

    JS Assignment

---
+++ Desktop/Henry Ilozumba JS Assignment/asset/js/script.js
@@ -0,0 +1,9 @@
+// const bio = document.getElementById("bio");
+// const updateButton = document.getElementById("update-button");
+
+// updateButton.addEventListener("click", function() {
+//   bio.textContent = "I am currently learning JavaScript";
+// });
+function changeUpdateButton() {
+  const changeUpdateMe = document.querySelector("#updateButton").textContent = "I am Learning Javascript"
+}
